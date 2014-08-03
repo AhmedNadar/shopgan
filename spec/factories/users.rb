789891573546ac8f8696179e9e2a170a2ea158a0
Ahmed Nadar <ahmedfay@hotmail.com>
2014-08-03 08:59:12 -0400

    Update Sign in spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
     username 		{Faker::Internet.user_name}
     email 			{Faker::Internet.email}
     password 			{Faker::Internet.password(8)}
-    password_confirmation {Faker::Internet.password(8)}
+    password_confirmation {password}
   end
 end
 
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 feature "Sign in as a user" do
   scenario "with right credentials" do
     user = FactoryGirl.build(:user)
-    visit new_user_registration_path
-
+    visit root_path
+    click_link 'Create Account'
+    
     expect(current_path).to eq new_user_registration_path
     
     fill_in 'user_first_name',              with: user.first_name
@@ -15,10 +16,21 @@ feature "Sign in as a user" do
     fill_in 'user_password_confirmation',   with: user.password_confirmation
     click_button 'Sign up'
 
-    # expect(current_path).to eq root_path
+    expect(current_path).to eq dashboard_path
 
     expect(User.count).to eq(1)
     expect(page).to have_content 'Welcome! You have signed up successfully.'
     expect(page).to have_link 'Sign out',       href: destroy_user_session_path
   end
 end
+
+  #   fill_in "Email", with: user.email
+  #   fill_in "Password", with: user.password
+  #   fill_in "Password confirmation", with: user.password_confirmation
+  #   click_button "Sign up"
+
+  #   expect(current_path).to eq playlists_path 
+  #   expect(page).to have_css '.alert',          text: 'Welcome! You have signed up successfully.'  
+  #   expect(page).to have_link 'Sign out',       href: destroy_user_session_path
+  #   expect(page).to have_link 'Edit profile',   href: edit_user_registration_path
+  # 
