d3c972ee80ceaec906f6ba0f2ce168303ef3b8ba
Ahmed Nadar <ahmedfay@hotmail.com>
2014-08-04 23:16:15 -0400

    Start my product spec but no clue to finish it

---
+++ spec/features/view_aproduct_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature "User visit dashboard" do
+  scenario "should see product information" do
+    # visit dashboard_path
+
+    # within 'h2' do
+    # 	expect(page)to have
+    end
+  end
+end
