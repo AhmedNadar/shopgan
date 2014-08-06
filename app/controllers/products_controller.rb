ec2a776d7d2a1886df1883f2d09920f2a95e0ddd
Ahmed Nadar <ahmedfay@hotmail.com>
2014-08-06 00:37:47 -0400

    Add Edit, Destroy and Update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController <ApplicationController
+
+	before_action :set_products, only: [:show, :update, :edit, :destroy]
+
 	def index
 		@products = Product.all
 	end
 
-	def show
-		@product = Product.find(params[:id])	
+	def show	
 	end
 
 	def new
@@ -22,23 +24,27 @@ class ProductsController <ApplicationController
 		end		
 	end
 
-	# def edit
-		
-	# end
+	def edit		
+	end
 
-	# def update
-	# 	if @product.update(product_params)
- #  			redirect_to @product, notice: 'product was successfully uopdated.'
- #  		else
- #  			render :edit
- #  		end
-	# end
+	def update
+		if @product.update(product_params)
+  			redirect_to @product, notice: 'product was successfully uopdated.'
+  		else
+  			render :edit
+  		end
+	end
 
-	# def destroy
-		
-	# end
+	def destroy
+		@product.destroy
+		redirect_to products_path
+	end
 
 	private
+
+	def set_products
+		@product = Product.find(params[:id])
+	end
 	def product_params
 		params.require(:product).permit(:title, :description, :price, :image_url )
 	end
